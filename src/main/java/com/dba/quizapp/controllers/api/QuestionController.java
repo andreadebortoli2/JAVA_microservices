09cdf3f3e5f67232004e42c1c3a9bba3371fdf43
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-11-02 19:47:02 +0100

    edit rest controller and service with http status in response

--- src/main/java/com/dba/quizapp/controllers/api/QuestionController.java
+++ src/main/java/com/dba/quizapp/controllers/api/QuestionController.java
@@ -8,6 +8,7 @@ import com.dba.quizapp.service.QuestionService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,40 +25,28 @@ public class QuestionController {
     QuestionService service;
 
     @GetMapping("allQuestions")
-    public List<Questions> getAllQiestions() {
+    public ResponseEntity<List<Questions>> getAllQiestions() {
         return service.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Questions> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Questions>> getQuestionsByCategory(@PathVariable String category) {
         return service.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Questions question) {
-        if (service.addOrUpdateQuestion(question) == "success") {
-            return "Question added";
-        } else {
-            return "something went wrong, qestion not added";
-        }
+    public ResponseEntity<String> addQuestion(@RequestBody Questions question) {
+        return service.addOrUpdateQuestion(question);
     }
 
     @PutMapping("update")
-    public String updateQuestion(@RequestBody Questions question) {
-        if (service.addOrUpdateQuestion(question) == "success") {
-            return "Question updated";
-        } else {
-            return "something went wrong, qestion not updated";
-        }
+    public ResponseEntity<String> updateQuestion(@RequestBody Questions question) {
+        return service.addOrUpdateQuestion(question);
     }
 
     @DeleteMapping("delete/{id}")
-    public String deleteQuestion(@PathVariable int id) {
-        if (service.deleteQuestion(id) == "success") {
-            return "Question deleted";
-        } else {
-            return "something went wrong, qestion not deleted";
-        }
+    public ResponseEntity<String> deleteQuestion(@PathVariable int id) {
+        return service.deleteQuestion(id);
     }
 
 }
--- src/main/java/com/dba/quizapp/service/QuestionService.java
+++ src/main/java/com/dba/quizapp/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.dba.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.dba.quizapp.model.Questions;
@@ -14,22 +17,43 @@ public class QuestionService {
     @Autowired
     QuestionRepo repo;
 
-    public List<Questions> getAllQuestions() {
-        return repo.findAll();
+    public ResponseEntity<List<Questions>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Questions> getQuestionsByCategory(String category) {
-        return repo.findByCategory(category);
+    public ResponseEntity<List<Questions>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(repo.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addOrUpdateQuestion(Questions question) {
-        repo.save(question);
-        return "success";
+    public ResponseEntity<String> addOrUpdateQuestion(Questions question) {
+        try {
+            repo.save(question);
+            return new ResponseEntity<>("success", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("error", HttpStatus.BAD_REQUEST);
     }
 
-    public String deleteQuestion(int id) {
-        repo.deleteById(id);
-        return "success";
+    public ResponseEntity<String> deleteQuestion(int id) {
+        try {
+            repo.deleteById(id);
+            return new ResponseEntity<>("success", HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("error", HttpStatus.BAD_REQUEST);
     }
 
 }
